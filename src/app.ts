8babfe3293e09706059ad52c0d2e7919140442d5
naveenKumar0408 <naveenkumar19981119@gmail.com>
2023-02-09 19:44:15 +0530

    Render Project List Section

--- src/app.ts
+++ src/app.ts
@@ -58,6 +58,36 @@ function autobind(
   };
   return adjustedMethod;
 }
+
+//  Project List Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  constructor(private type: "active" | "finished") {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-list")!
+    );
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + "PROJECTS";
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
 // Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -145,3 +175,5 @@ class ProjectInput {
 }
 
 const projectInputObj = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
